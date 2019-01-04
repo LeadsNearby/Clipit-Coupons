355a1168c099562b8fb176b163a2f153ffa167cb
Andrew Gillingham <andrew@leadsnearby.com>
2019-01-04 12:58:52 -0500

    Move script and style enqueues to only pages where they are necessary

--- lib/shortcodes/clipit-shortcode.php
+++ lib/shortcodes/clipit-shortcode.php
@@ -39,6 +39,15 @@ function shortcode_clipit_coupons($atts) {
         $columns_words = 'col-1-4';
     }
 
+    wp_enqueue_style('clipit-styles');
+    wp_enqueue_style('clipit-print-styles');
+    wp_enqueue_script('jquery-ui-tooltip');
+    wp_enqueue_script('jquery-ui-dialog');
+    wp_enqueue_script('jquery-ui-tabs');
+    wp_enqueue_script('coupon-commons');
+    wp_enqueue_script('g-plusone');
+    wp_enqueue_script('countdown');
+
     $html = '';
     $html .= '<div id="clipit" class="coupons grid clipshort clipit_shortcode ' . $class . '">';
     $html .= '<div itemtype="http://schema.org/Offer" itemscope="">';
@@ -129,6 +138,8 @@ function shortcode_clipit_coupons($atts) {
                 );
                 echo clipit_render_single_coupon($post, 'multi', $to_be_deprecated);
             }
+
+            wp_enqueue_style('jquery-ui-styles');
 
             $secondsbetween = strtotime($expirationtime) - time();
             if ($secondsbetween > 0) {
