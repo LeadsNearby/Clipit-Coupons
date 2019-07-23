66a34decd339c01341b605bdba78e3c37532ac1f
Andrew Gillingham <andrew@leadsnearby.com>
2019-07-23 13:24:46 -0400

    wip templating

--- lib/templates/archive-coupon.php
+++ lib/templates/archive-coupon.php
@@ -2,4 +2,13 @@
 
 get_header();
 
+echo '<div id="clipit" class="clipit-coupons">';
+
+if (have_posts()): while (have_posts()): the_post();
+        include 'parts/coupon.php';
+    endwhile;
+endif;
+
+echo '</div>';
+
 get_footer();
---
+++ lib/templates/parts/coupon.php
@@ -0,0 +1,3 @@
+<article class="clipit-coupon">
+    <span class="clipit-coupon__title"><?php the_title();?></span>
+</article>
