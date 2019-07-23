199e52460833e365fd7c70b7f48692707685bfbf
Andrew Gillingham <andrew@leadsnearby.com>
2019-07-23 12:23:01 -0400

    Change template for archive

--- lib/templates/template-functions.php
+++ lib/templates/template-functions.php
@@ -1,38 +1,15 @@
 <?php
 // Template Functions
-	// Adds Template files when plugin is activated.
-	add_filter( 'template_include', 'coupon_template_function', 1 );
-	function coupon_template_function( $template_path ) {
-		if ( get_post_type() == 'coupon' ) {			
-			if ( is_single() ) {
-				// checks if the file exists in the theme first,
-				// otherwise serve the file from the plugin
-				if ( $theme_file = locate_template( array ( 'single-coupon.php' ) ) ) {
-					$template_path = $theme_file;
-				} else {
-					$template_path = plugin_dir_path( __FILE__ ) . 'single-coupon.php';
-				}
-			}
-			// Will be used in later versions!
-			//if ( is_single() ) {
-				// checks if the file exists in the theme first,
-				// otherwise serve the file from the plugin
-				//if ( $theme_comments_file = locate_template( array ( 'clipit-comments.php' ) ) ) {
-					//$comments_path = $theme_comments_file;
-				//} else {
-					//$comments_path = plugin_dir_path( __FILE__ ) . 'clipit-comments.php';
-				//}
-			//}
-			if ( is_archive() ) {
-				// checks if the file exists in the theme first,
-				// otherwise serve the file from the plugin
-				if ( $theme_file = locate_template( array ( 'taxonomy-locations.php' ) ) ) {
-					$template_path = $theme_file;
-				} else {
-					$template_path = plugin_dir_path( __FILE__ ) . 'taxonomy-locations.php';
-				}
-			}
-		}
-		return $template_path;
-	}
-?>
+// Adds Template files when plugin is activated.
+add_filter('template_include', 'coupon_template_function', 1);
+function coupon_template_function($template_path) {
+    if (get_post_type() == 'coupon') {
+        if (is_single()) {
+            $template_path = plugin_dir_path(__FILE__) . 'single-coupon.php';
+        }
+        if (is_archive()) {
+            $template_path = plugin_dir_path(__FILE__) . 'coupon-archive.php';
+        }
+    }
+    return $template_path;
+}
