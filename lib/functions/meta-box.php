1d2f9d31f56772d1d0f352052fd374f7f4e6aba3
Andrew Gillingham <andrew@leadsnearby.com>
2019-07-23 10:13:14 -0400

    wip trim meta-box, options

--- lib/functions/meta-box.php
+++ lib/functions/meta-box.php
@@ -36,22 +36,19 @@ function coupon_options( $post ) {
 	wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
     ?>
 
-	<div class="tab">
+	<!-- <div class="tab">
 	  <div class="tablinks" onclick="openOption(event, 'Actions')" id="defaultOpen">Actions</div>
 	  <div class="tablinks" onclick="openOption(event, 'Fine')">Fine Print & Promo</div>
 	  <div class="tablinks" onclick="openOption(event, 'Styles')">Styles</div>
-	</div>
+	</div> -->
 
-	<div id="Actions" class="tabcontent">
-	  <h3>Coupon Actions</h3>
+	<div id="Actions" class="">
 	  <p>
 	    <label for="coupon_action"><?php _e( 'Coupon Action', 'inputname' ); ?>:</label><br />
 	    <em>Select what action you would like your coupon to take when a user clicks on it.</em><br />
 	    <select name="coupon_action" id="coupon_action">
 	        <option value="url" <?php selected( $coupon_action, 'url' ); ?>>Destination URL</option>
 	        <option value="print" <?php selected( $coupon_action, 'print' ); ?>>Print Page</option>
-	        <option value="promo" <?php selected( $coupon_action, 'promo' ); ?>>Promo Code Display</option>
-	        <option value="peel" <?php selected( $coupon_action, 'peel' ); ?>>Promo Code Peel</option>
 	    </select>
 	  </p>
 	  <p class="url"><label for="coupon_destination_url"><?php _e( 'Destination URL', 'inputname' ); ?>:</label><br />
@@ -63,41 +60,12 @@ function coupon_options( $post ) {
 	    <input id="datepicker" class="coupon-text" type="text" size="" name="coupon_expiration" value="<?php echo esc_html($coupon_expiration); ?>" /> 
 	  </p>
 	  <p>
-	    <input type="checkbox" id="coupon_dynamic_expiration" name="coupon_dynamic_expiration" <?php checked( $coupon_dynamic_expiration, 'on' ); ?> />
-	    <label for="coupon_dynamic_expiration"><?php _e( 'Enable Dynamic Expiration Date', 'inputname' ); ?></label><br />
-	    <em>Check to enable the Dynamic Expiration Date Feature</em>
-	  </p>
-	  <p>
-	    <label for="coupon_dynamic_expiration_plus_days"><?php _e( 'Dynamic Expiration + Days', 'inputname' ); ?>:</label><br />
-	    <em>Select how many days in the future you want dynamic expiration to show.</em><br />
-	    <select name="coupon_dynamic_expiration_plus_days" id="coupon_dynamic_expiration_plus_days">
-	        <option value="+0 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+0 day' ); ?>>+0 day</option>
-	        <option value="+1 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+1 day' ); ?>>+1 day</option>
-	        <option value="+2 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+2 day' ); ?>>+2 day</option>
-	        <option value="+3 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+3 day' ); ?>>+3 day</option>
-	        <option value="+4 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+4 day' ); ?>>+4 day</option>
-	        <option value="+5 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+5 day' ); ?>>+5 day</option>
-	        <option value="+6 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+6 day' ); ?>>+6 day</option>
-	        <option value="+7 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+7 day' ); ?>>+7 day</option>
-	        <option value="+8 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+8 day' ); ?>>+8 day</option>
-	        <option value="+9 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+9 day' ); ?>>+9 day</option>
-	        <option value="+10 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+10 day' ); ?>>+10 day</option>
-	        <option value="+11 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+11 day' ); ?>>+11 day</option>
-	        <option value="+12 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+12 day' ); ?>>+12 day</option>
-	        <option value="+13 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+13 day' ); ?>>+13 day</option>
-	        <option value="+14 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+14 day' ); ?>>+14 day</option>
-	    </select>
-	  </p>
-	</div>
-	<div id="Fine" class="tabcontent">
-	  <h3>Fine Print and Promotions</h3>
-	  <p>
 	    <label for="coupon_fineprint"><?php _e( 'The Fine Print', 'textfine' ); ?>:</label><br />
 	    <em>Enter your coupon fine print information here.</em><br />
 	        <?php $fineprint_textarea = get_post_meta($post->ID, 'coupon_fineprint', true);
 	    wp_editor($fineprint_textarea, 'coupon_fineprint', array(
 	      'wpautop' => true,
-	      'media_buttons' => true,
+	      'media_buttons' => false,
 	      'textarea_name' => 'coupon_fineprint',
 	      'textarea_rows' => 10,
 	      'teeny' => true
@@ -105,18 +73,6 @@ function coupon_options( $post ) {
 	    ?>    
 	    <em><?php _e( 'Suitable for text and HTML. May include %s tags.', 'coupons' ); ?></em>
 	  </p>
-	  <p><label for="coupon_promo_code"><?php _e( 'Coupon Code/Promotional Code', 'inputname' ); ?>:</label><br />
-	    <em>Enter your Coupon or Promo Code here. This will appear if you select "Promo Code Display" or partially appear if you select "Promo Code Peel" from the Coupon Actions drop down</em><br />
-	    <input class="coupon_promo_code" type="text" size="" name="coupon_promo_code" value="<?php echo esc_html($coupon_promo_code); ?>" /> 
-	  </p>
-	  <p><label>Promo Text</label><br />
-	    <em>If you selected "Promo Code Peel" from the Coupon Actions dropdown this text will appear in the modal that opens up to display the promo code.</em><br />
-	    <input class="coupon_promo_text" type="text" size="" name="coupon_promo_text" value="<?php echo esc_html($coupon_promo_text); ?>" /> 
-	  </p>
-	</div>
-
-	<div id="Styles" class="tabcontent">
-	  <h3>Styles</h3>
 	  <p><label>Custom Button Text</label><br />
 	    <em>Enter in your custom button text. This will override the default button text</em><br />
 	    <input class="coupon_button_text" type="text" size="" name="coupon_button_text" value="<?php echo esc_html($coupon_button_text); ?>" /> 
