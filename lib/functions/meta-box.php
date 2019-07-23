8d61df9e8115a7a0991b92a5edb8cca4b81b4948
Andrew Gillingham <andrew@leadsnearby.com>
2019-07-23 10:05:45 -0400

    wip trim meta-box, options

--- lib/functions/meta-box.php
+++ lib/functions/meta-box.php
@@ -24,12 +24,9 @@ function coupon_options( $post ) {
 	$coupon_dynamic_expiration = (isset( $values['coupon_dynamic_expiration'] ) ? esc_attr( $values['coupon_dynamic_expiration'][0] ) : '');
 	$coupon_dynamic_expiration_plus_days = (isset( $values['coupon_dynamic_expiration_plus_days'] ) ? esc_attr( $values['coupon_dynamic_expiration_plus_days'][0] ) : '');
 	$coupon_expiration = (isset( $values['coupon_expiration'] ) ? esc_attr( $values['coupon_expiration'][0] ) : '');
-	$coupon_display_expiration = (isset( $values['coupon_display_expiration'] ) ? esc_attr( $values['coupon_display_expiration'][0] ) : '');
 	$coupon_destination_url = (isset( $values['coupon_destination_url'] ) ? esc_attr( $values['coupon_destination_url'][0] ) : '');
 	$coupon_action = (isset( $values['coupon_action'] ) ? esc_attr( $values['coupon_action'][0] ) : '');
 	$coupon_type = (isset( $values['coupon_type'] ) ? esc_attr( $values['coupon_type'][0] ) : '');
-	$coupon_social_like = (isset( $values['coupon_social_like'] ) ? esc_attr( $values['coupon_social_like'][0] ) : '');
-	$coupon_like = (isset( $values['coupon_like'] ) ? esc_attr( $values['coupon_like'][0] ) : '');
 	$coupon_promo_code = (isset( $values['coupon_promo_code'] ) ? esc_attr( $values['coupon_promo_code'][0] ) : '');
 	$coupon_name  = (isset( $values['coupon_name'] ) ? esc_attr( $values['coupon_name'][0] ) : '');
 	$coupon_fineprint = (isset( $values['coupon_fineprint'] ) ? esc_attr( $values['coupon_fineprint'][0] ) : '');
@@ -41,21 +38,12 @@ function coupon_options( $post ) {
 
 	<div class="tab">
 	  <div class="tablinks" onclick="openOption(event, 'Actions')" id="defaultOpen">Actions</div>
-	  <div class="tablinks" onclick="openOption(event, 'Options')">Options</div>
 	  <div class="tablinks" onclick="openOption(event, 'Fine')">Fine Print & Promo</div>
 	  <div class="tablinks" onclick="openOption(event, 'Styles')">Styles</div>
 	</div>
 
 	<div id="Actions" class="tabcontent">
 	  <h3>Coupon Actions</h3>
-	  <p>
-	    <label for="coupon_type"><?php _e( 'Coupon Type', 'inputname' ); ?>:</label><br />
-	    <em>Select if you want to upload your coupons or build them.</em><br />
-	    <select name="coupon_type" id="coupon_type">
-	        <option value="Build" <?php selected( $coupon_type, 'Build' ); ?>>Build Coupon</option>
-	        <option value="Upload" <?php selected( $coupon_type, 'Upload' ); ?>>Upload Coupon</option>
-	    </select>
-	  </p>
 	  <p>
 	    <label for="coupon_action"><?php _e( 'Coupon Action', 'inputname' ); ?>:</label><br />
 	    <em>Select what action you would like your coupon to take when a user clicks on it.</em><br />
@@ -100,39 +88,6 @@ function coupon_options( $post ) {
 	        <option value="+14 day" <?php selected( $coupon_dynamic_expiration_plus_days, '+14 day' ); ?>>+14 day</option>
 	    </select>
 	  </p>
-	</div>
-
-	<div id="Options" class="tabcontent">
-	  <h3>Coupon Options</h3>
-	  <p>
-	    <input type="checkbox" id="coupon_display_expiration" name="coupon_display_expiration" <?php checked( $coupon_display_expiration, 'on' ); ?> />
-	    <label for="coupon_display_expiration"><?php _e( 'Display Expiration Date', 'inputname' ); ?></label><br />
-	    <em>Check to display Expiration Date Feature</em>
-	  </p>  
-	  <p>
-	    <input type="checkbox" id="coupon_social" name="coupon_social" <?php checked( $coupon_social, 'on' ); ?> />
-	    <label for="coupon_social"><?php _e( 'Social Sharing', 'inputname' ); ?></label><br />
-	    <em>Check to enable the Social Sharing</em>
-	  </p>
-	  <p>
-	    <input type="checkbox" id="coupon_email" name="coupon_email" <?php checked( $coupon_email, 'on' ); ?> />
-	    <label for="coupon_email"><?php _e( 'Email to Claim Coupon', 'inputname' ); ?></label><br />
-	    <em>Check to enable the Email to Claim Feature</em>
-	  </p>
-	  <p>
-	    <input type="checkbox" id="coupon_feature" name="coupon_feature" <?php checked( $coupon_feature, 'on' ); ?> />
-	    <label for="coupon_feature"><?php _e( 'Featured Coupon', 'inputname' ); ?></label><br />
-	    <em>Check to enable the Featured Coupon Banner</em>
-	  </p>
-	  <p>
-	    <input type="checkbox" id="coupon_views" name="coupon_views" <?php checked( $coupon_views, 'on' ); ?> />
-	    <label for="coupon_views"><?php _e( 'Display Coupon Views', 'inputname' ); ?></label><br />
-	    <em>Check to display views of coupon</em>
-	  </p>
-	  <p><label for="coupon_name"><?php _e( 'Coupon Name', 'inputname' ); ?>:</label><br />
-	    <em>Enter the name of your coupon - for internal use only.</em><br />
-	    <input class="coupon_name" type="text" size="" name="coupon_name" value="<?php echo esc_html($coupon_name); ?>" /> 
-	  </p> 
 	</div>
 	<div id="Fine" class="tabcontent">
 	  <h3>Fine Print and Promotions</h3>
@@ -255,37 +210,15 @@ function cd_meta_box_save( $post_id )
 		
 	if( isset( $_POST['coupon_css_class'] ) )
 		update_post_meta( $post_id, 'coupon_css_class', $_POST['coupon_css_class'] );
-
-	if( isset( $_POST['coupon_promo_text'] ) )
-		update_post_meta( $post_id, 'coupon_promo_text', wp_kses( $_POST['coupon_promo_text'], $allowed ) );
 	
 	if( isset( $_POST['coupon_button_text'] ) )
 		update_post_meta( $post_id, 'coupon_button_text', wp_kses( $_POST['coupon_button_text'], $allowed ) );
-		
-
-	$dynamic_expiration = isset( $_POST['coupon_dynamic_expiration'] ) && $_POST['coupon_dynamic_expiration'] ? 'on' : 'off';
-	update_post_meta( $post_id, 'coupon_dynamic_expiration', $dynamic_expiration );
 	
 	$display_expiration = isset( $_POST['coupon_display_expiration'] ) && $_POST['coupon_display_expiration'] ? 'on' : 'off';
 	update_post_meta( $post_id, 'coupon_display_expiration', $display_expiration );
 	
-	$chk = isset( $_POST['coupon_feature'] ) && $_POST['coupon_feature'] ? 'on' : 'off';
-	update_post_meta( $post_id, 'coupon_feature', $chk );
-	
-	$socialchk = isset( $_POST['coupon_social'] ) && $_POST['coupon_social'] ? 'on' : 'off';
-	update_post_meta( $post_id, 'coupon_social', $socialchk );
-	
 	$claim = isset( $_POST['coupon_email'] ) && $_POST['coupon_email'] ? 'on' : 'off';
 	update_post_meta( $post_id, 'coupon_email', $claim );
-	
-	$social_like = isset( $_POST['coupon_social_like'] ) && $_POST['coupon_social_like'] ? 'on' : 'off';
-	update_post_meta( $post_id, 'coupon_social_like', $social_like );
-
-	$like = isset( $_POST['coupon_like'] ) && $_POST['coupon_like'] ? 'on' : 'off';
-	update_post_meta( $post_id, 'coupon_like', $like );
-
-	$views = isset( $_POST['coupon_views'] ) && $_POST['coupon_views'] ? 'on' : 'off';
-	update_post_meta( $post_id, 'coupon_views', $views );
 	   
 }
 ?>
