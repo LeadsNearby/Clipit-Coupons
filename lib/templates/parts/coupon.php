edd019c9e60adfe220663db86cc983992f0d7b97
Andrew Gillingham <andrew@leadsnearby.com>
2019-07-23 15:03:24 -0400

    wip templating

--- lib/templates/parts/coupon.php
+++ lib/templates/parts/coupon.php
@@ -1,6 +1,7 @@
 <article class="clipit-coupon">
-    <span class="clipit-coupon__title"><?php esc_html(the_title());?></span>
+    <?php include 'coupon-title.php';?>
     <span class="clipit-coupon__subtitle"><?php esc_html(the_excerpt());?></span>
+    <span class="clipit-coupon__spacer"></span>
     <span class="clipit-coupon__expiration"></span>
     <span class="clipit-coupon__act"></span>
     <a class="clipit-coupon__button" href="<?php the_permalink();?>">View Details</a>
