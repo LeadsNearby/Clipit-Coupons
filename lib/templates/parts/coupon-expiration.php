e07d01cddf326c5b929ade50f4a62f69f402875d
Andrew Gillingham <andrew@leadsnearby.com>
2019-07-24 11:26:36 -0400

    wip templating

---
+++ lib/templates/parts/coupon-expiration.php
@@ -0,0 +1,29 @@
+<?php
+
+$coupon_expiration = get_post_meta(get_the_ID(), 'coupon_expiration', true);
+$coupon_expiration_text = 'Available for a Limited Time';
+$act_text = 'Act now!';
+
+if ($coupon_expiration) {
+    $expiration = date_create(date('Y-m-d', strtotime($coupon_expiration)));
+    $current_date = date_create(date('Y-m-d'));
+
+    $interval = date_diff($current_date, $expiration);
+
+    $diff = $interval->format('%a');
+
+    $days = 'days';
+    if ($diff < 2) {
+        $days = 'day';
+    }
+
+    if ($diff > 0 && $diff < 16) {
+        $coupon_expiration_text = 'Expires: ' . $coupon_expiration;
+        $act_text = 'Act now, ' . $diff . ' ' . $days . ' left!';
+    }
+}
+
+?>
+
+<span class="clipit-coupon__expiration"><?php echo $coupon_expiration_text; ?></span>
+<span class="clipit-coupon__act"><?php echo $act_text; ?></span>
--- lib/templates/parts/coupon.php
+++ lib/templates/parts/coupon.php
@@ -1,8 +1,11 @@
+<?php wp_enqueue_style('arvo', 'https://fonts.googleapis.com/css?family=Arvo:700&display=swap');?>
 <article class="clipit-coupon">
+    <span class=clipit-coupon__save-wrapper>
+        <span class="clipit-coupon__save">Save</span>
+    </span>
     <?php include 'coupon-title.php';?>
     <span class="clipit-coupon__subtitle"><?php esc_html(the_excerpt());?></span>
     <span class="clipit-coupon__spacer"></span>
-    <span class="clipit-coupon__expiration"></span>
-    <span class="clipit-coupon__act"></span>
+    <?php include 'coupon-expiration.php';?>
     <a class="clipit-coupon__button" href="<?php the_permalink();?>">View Details</a>
 </article>
