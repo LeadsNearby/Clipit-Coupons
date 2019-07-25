eb92ab464f5fc3ea1b92f1b86e66e2207a7e5b70
Andrew Gillingham <andrew@leadsnearby.com>
2019-07-25 13:24:56 -0400

    wip

--- lib/templates/single-coupon.php
+++ lib/templates/single-coupon.php
@@ -10,10 +10,11 @@ echo '<div id="clipit" class="clipit-coupons clipit-coupons--single">';
 
 if (have_posts()):
     while (have_posts()): the_post();
-        $coupon_expiration = get_post_meta($post->ID, 'coupon_expiration', true);
-        $coupon_fineprint = get_post_meta($post->ID, 'coupon_fineprint', true) ? get_post_meta($post->ID, 'coupon_fineprint', true) : get_option('clipit_fineprint_default', '');
-        $coupon_shorts = get_post_meta($post->ID, 'coupon_shorts', true);
-        $coupon_css_class = get_post_meta($post->ID, 'coupon_css_class', true);
+        $post_id = get_the_ID();
+        $coupon_expiration = get_post_meta($post_id, 'coupon_expiration', true);
+        $coupon_fineprint = get_post_meta($post_id, 'coupon_fineprint', true) ? get_post_meta($post_id, 'coupon_fineprint', true) : get_option('clipit_fineprint_default', '');
+        $coupon_shorts = get_post_meta($post_id, 'coupon_shorts', true);
+        $coupon_css_class = get_post_meta($post_id, 'coupon_css_class', true);
         $unix_coupon_expiration = strtotime($coupon_expiration . ' 11:59 pm');
         include 'parts/coupon-single.php';
     endwhile;
